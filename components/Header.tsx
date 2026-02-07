7ea5302eebe36d30d816a551c029d3af3c031228
Muhamad Renald <avryso@gmail.com>
2026-02-08 05:11:32 +0700

    style(header): update fontSize

--- components/Header.tsx
+++ components/Header.tsx
@@ -33,7 +33,7 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   title: {
-    fontSize: 29,
+    fontSize: 30,
     fontWeight: "800",
     color: COLOR.secondary,
     letterSpacing: 5,
